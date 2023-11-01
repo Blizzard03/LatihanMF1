58dd5c126eddcf60e6f6521ef0f6a87e2220cc74
Blizzard03 <99153189+Blizzard03@users.noreply.github.com>
2023-11-01 11:10:54 +0700

    update fxmloutputcontroller

--- src/main/java/com/mariq/rasyid/latihanmf1/FXMLOutputController.java
+++ src/main/java/com/mariq/rasyid/latihanmf1/FXMLOutputController.java
@@ -25,7 +25,7 @@ public class FXMLOutputController implements Initializable {
 
     Locale Indonesia = new Locale("in", "ID");
     NumberFormat formater = NumberFormat.getCurrencyInstance(Indonesia);
-    
+
     @FXML
     private Label custumername;
     @FXML
@@ -54,20 +54,21 @@ public class FXMLOutputController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-    
+
     @FXML
     private void close_btn_click(ActionEvent event) {
         System.exit(0);
     }
-    
+
     @FXML
     private void Backbtnclick(ActionEvent event) {
         Backbtn.getScene().getWindow().hide();
     }
-    
+
     public void getValue(FunitureModel mdl) {
         String Funiture = "";
         double Price = 0, Discount = 0;
+
         switch (mdl.getFunituretype()) {
             case 1: {
                 Funiture = "Cupboard";
@@ -95,16 +96,19 @@ public class FXMLOutputController implements Initializable {
                 Funiture = "Chair";
                 if (mdl.getCategory() == 1) {
                     Price = 200000;
-                    if (Price <= 10000000) {
-                        Discount = Price * 0.1;
+                    double total_price = Price * mdl.getQty();
+                    if (total_price > 10000000) {
+                        Discount = (total_price * 0.1);
                     } else {
                         Discount = 0;
                     }
-                    
+
                 } else if (mdl.getCategory() == 2) {
                     Price = 350000;
-                    if (Price <= 10000000) {
-                        Discount = Price * 0.1;
+                    double total_price = Price * mdl.getQty();
+                    int Quantity = mdl.getQty();
+                    if (total_price >= 10000000) {
+                        Discount = (total_price * 0.1);
                     } else {
                         Discount = 0;
                     }
@@ -114,23 +118,26 @@ public class FXMLOutputController implements Initializable {
             default: {
                 Funiture = "404 NOT FOUND";
                 Price = 0;
-               Discount = 0;
+                Discount = 0;
                 break;
             }
         }
-        
         double total_price = Price * mdl.getQty();
         double SumTotal = total_price - Discount;
         custumername.setText(mdl.getCustumername());
         funituretype.setText(Funiture);
-        qty.setText(String.valueOf(mdl.getQty()));
+        if (mdl.getCategory() == 1) {
+            qty.setText("Common");
+        } else {
+            qty.setText("Luxury");
+        }
+
         qty_total.setText(String.valueOf(mdl.getQty()));
         price.setText(formater.format(Price));
         sumprice.setText(formater.format(total_price));
         discount.setText(formater.format(Discount));
         totalprice.setText(formater.format(SumTotal));
-        
-        
+
     }
-    
+
 }
